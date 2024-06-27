11ba3a9c49fd187e5218fa6703a17a76721ff4af
Matija Krajnik <matija.krajnik90@gmail.com>
2024-06-27 12:58:56 +0200

    chapter 5 done

---
+++ chapter_5/pento/lib/pento/catalog/product.ex
@@ -0,0 +1,30 @@
+defmodule Pento.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :name, :string
+    field :description, :string
+    field :unit_price, :float
+    field :sku, :integer
+    field :image_upload, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:name, :description, :unit_price, :sku, :image_upload])
+    |> validate_required([:name, :description, :unit_price, :sku])
+    |> unique_constraint(:sku)
+    |> validate_number(:unit_price, greater_than: 0.0)
+  end
+
+  def lower_price_changeset(product, attrs) do
+    product
+    |> cast(attrs, [:unit_price])
+    |> validate_required([:unit_price])
+    |> validate_number(:unit_price, lesser_than: product.unit_price)
+  end
+end
